73c39a6d339f6b9328e1d1bb1c72d0cccc0117fd
justflying <jonglae@gmail.com>
2019-05-11 21:20:22 +0900

    기능추가    - 리비젼 리스트 클릭스 이번회차 매칭 번호 출력

--- app/src/test/java/antest/lotto_test/mongo.java
+++ app/src/test/java/antest/lotto_test/mongo.java
@@ -11,6 +11,15 @@ public class mongo {
 
         assert rand != null;
         int bbbb = rand.nextInt(7 - 1 + 1) + 1;
+
+
+        res[0] = random.nextInt(7 - 1 + 1) + 1;
+        res[1] = random.nextInt(16 - 8 + 1) + 8;
+        res[2] = random.nextInt(24 - 17 + 1) + 17;
+        res[3] = random.nextInt(33 - 25 + 1) + 25;
+        res[4] = random.nextInt(41 - 34 + 1) + 34;
+        res[5] = random.nextInt(46 - 42 + 1) + 42;
+        
         System.out.println ("===bbbb== : "+bbbb);
     }
 
